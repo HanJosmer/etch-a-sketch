4b01b53d4e558990b6a2fc6753342f2839a754e9
John Hosmer <john.n.hosmer@gmail.com>
2018-09-14 16:51:07 -0700

    Write resetGrid() function

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-let elSketch = document.querySelector("#sketch");
-let size = prompt("How many cells to a side?");
-
-// populate sketch area with divs
-for (let i = 0; i < size; i++) {
-    elRow = document.createElement("div");
-    elRow.classList.add("row-container")
-    elSketch.appendChild(elRow);
-    for (let j = 0; j < size; j++) {
-        elCell = document.createElement("div");
-        elCell.classList.add("row-cell");
-        elCell.addEventListener('mouseover', (e) => {
-            e.target.classList.add("active");
-        });
-        elRow.appendChild(elCell);
-    }
-}
+buildGrid();
 
 let elLeftButton = document.querySelector("#left-button");
 let elMiddleButton = document.querySelector("#middle-button");
@@ -25,15 +9,49 @@ elMiddleButton.addEventListener('click', resetGrid);
 elRightButton.addEventListener('click', rainbowMode);
 
 
-function resetGrid() {
-    return alert("This feature has not been implemented yet");
+function buildGrid() {
+    let elSketch = document.querySelector("#sketch");
+    let size = prompt("How many cells to a side?");
+
+    // populate sketch area with divs
+    for (let i = 0; i < size; i++) {
+        elRow = document.createElement("div");
+        elRow.classList.add("row-container")
+        elSketch.appendChild(elRow);
+        for (let j = 0; j < size; j++) {
+            elCell = document.createElement("div");
+            elCell.classList.add("row-cell");
+            elCell.addEventListener('mouseover', (e) => {
+                e.target.classList.add("active");
+            });
+            elRow.appendChild(elCell);
+        };
+    };
 };
+
+function resetGrid() {
+    let elSketch = document.querySelector("#sketch");
+
+    while (elSketch.firstChild) {
+        elSketch.removeChild(elSketch.firstChild);
+    };
+
+    buildGrid();
+};
+
+function randomColor() {
+       let red = null;
+       let green = null;
+       let blue = null;
+
+       
+}
 
 function rainbowMode() {
     return alert("This feature has not been implemented yet");
-};
+}
 
 function gradientMode() {
-    return alert("This feature has not bee implemented yet");
+    return alert("This feature has not been implemented yet");
 }
 
