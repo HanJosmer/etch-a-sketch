8f68db8fd4623ff38cfe3a7a9b543dda48af0174
John Hosmer <john.n.hosmer@gmail.com>
2018-09-19 10:22:35 -0700

    Finish index.html, script.js, and style.css

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ elRightButton.addEventListener('click', rainbowMode);
 
 function buildGrid() {
     let elSketch = document.querySelector("#sketch");
-    let size = prompt("How many cells to a side?");
+    let size = prompt("Sketch size (pixels to a side)?");
 
     // populate sketch area with divs
     for (let i = 0; i < size; i++) {
@@ -21,9 +21,7 @@ function buildGrid() {
         for (let j = 0; j < size; j++) {
             elCell = document.createElement("div");
             elCell.classList.add("row-cell");
-            elCell.addEventListener('mouseover', (e) => {
-                e.target.classList.add("active");
-            });
+            elCell.addEventListener('mouseover', activateCell);
             elRow.appendChild(elCell);
         };
     };
@@ -39,19 +37,67 @@ function resetGrid() {
     buildGrid();
 };
 
-function randomColor() {
-       let red = null;
-       let green = null;
-       let blue = null;
+function activateCell(e) {
+    e.target.classList.add("active");
+};
 
+function randomColor() {
+
+    // generate random rgb values between 0 and 255
+    let red = Math.round((Math.random() * 255));
+    let green = Math.round((Math.random() * 255));
+    let blue = Math.round((Math.random() * 255));
        
+    return `rgb(${red},${green},${blue})`;
 }
+
+function rainbowCell(e) {
+    e.target.style.backgroundColor = randomColor();
+};
 
 function rainbowMode() {
-    return alert("This feature has not been implemented yet");
+
+    // remove initial event listeners and add gradient event listeners
+    let cells = document.querySelectorAll(".row-cell");
+    cells.forEach((cell) => {
+        cell.removeEventListener('mouseover', activateCell);
+        cell.removeEventListener('mouseover', gradientCell);
+        cell.addEventListener('mouseover', rainbowCell);
+    });
+};
+
+function gradientCell(e) {
+    e.target.style.backgroundColor = gradient(e);
 }
 
+function gradient(e) {
+
+    let style = e.target.style.backgroundColor;
+
+    if (!style) {
+        style = "rgba(0, 0, 0, 0.1)";
+    }
+    
+    else if (style && style.substr(0, 12) != "rgba(0, 0, 0") {
+        style = "rgba(0, 0, 0, 0.1)";
+    }
+
+    else {   
+        let alpha = style.substr(14, 3);
+        style = style.replace(alpha, `${+alpha + 0.1}`);
+    };
+
+    return style;
+};
+
 function gradientMode() {
-    return alert("This feature has not been implemented yet");
-}
+    
+    // remove existing event listeners and add gradient event listeners
+    let cells = document.querySelectorAll(".row-cell");
+    cells.forEach((cell) => {
+        cell.removeEventListener('mouseover', activateCell);
+        cell.removeEventListener('mouseover', rainbowCell);
+        cell.addEventListener('mouseover', gradientCell);
+    });
+};
 
